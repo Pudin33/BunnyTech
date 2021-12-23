40270ed289cc7f2e62e5391504bf8a1727b0c1b8
n0kuro <90203975+n0kuro@users.noreply.github.com>
2021-12-23 14:45:25 +0800

    Initialization of MLRSU controller

---
+++ MLRSUController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace elawExcel.Controllers
+{
+    public class MLRSUController : Controller
+    {
+        // GET: MLRSU
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
